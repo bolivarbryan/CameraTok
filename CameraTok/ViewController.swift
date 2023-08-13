598ad80a2fb90904173aa5e425377b603fa49507
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2023-08-13 12:26:57 -0500

    First Commit, Project Setup

---
+++ CameraTok/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CameraTok
+//
+//  Created by Bryan A Bolivar M on 13/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
